c0b3fe15a1e0ee12600003343752410e8cd71afd
dinkoosmankovic <dinko.osmankovic@etf.unsa.ba>
2016-07-30 00:36:09 +0200

    First commit for ETFSA_ATV gazebo simulator

---
+++ etfsa_atv_description/env/etfsa_atv_description.sh~
@@ -0,0 +1,1 @@
+export ETFSA_ATV_DESCRIPTION=$(catkin_find --without-underlays --first-only etfsa_atv_description urdf/description.xacro 2>/dev/null)
---
+++ etfsa_atv_gazebo/env_hooks/60.etfsa_atv_description.sh
@@ -0,0 +1,1 @@
+export ETFSA_ATV_GAZEBO_DESCRIPTION=$(catkin_find --without-underlays --first-only etfsa_atv_gazebo urdf/description.gazebo.xacro 2>/dev/null)
